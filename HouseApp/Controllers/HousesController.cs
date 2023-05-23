f60d22ea4ce5e322447f963630df8cd106996189
Estlib <75094683+Estlib@users.noreply.github.com>
2023-05-23 23:32:51 +0300

    started adding the controller, quickly realised i need interface too

---
+++ HouseApp/Controllers/HousesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HouseApp.Controllers
+{
+    public class HousesController : Controller
+    {
+        //constructor
+        public HousesController(HouseApp context, IHousesServices housesServices)
+        {
+            _context = context;
+            _housesServices = housesServices;
+        }
+
+        //indexview
+        public IActionResult Index()
+        {
+            var result = _context.Houses
+            return View(result);
+        }
+    }
+}
--- HouseApp/Models/House/HouseIndexViewModel.cs
+++ HouseApp/Models/House/HouseIndexViewModel.cs
@@ -1,6 +1,15 @@
-﻿namespace HouseApp.Models.House
+﻿using System.Drawing;
+
+namespace HouseApp.Models.House
 {
     public class HouseIndexViewModel
     {
+        public Guid? Id { get; set; }
+        public int BathroomCount { get; set; }
+        public int BedroomCount { get; set; }
+        public bool IsForRentOrSale { get; set; }
+        public int Price { get; set; }
+        public int BuildingAge { get; set; }
+
     }
 }
